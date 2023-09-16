a5bfa117d5992dc869a197c02bb7379002df64e5
sifat7252 <safikulislam.sifat@gmail.com>
2023-09-16 14:35:13 +0600

    added the card sections

--- src/component/Cards/cards.jsx
+++ src/component/Cards/cards.jsx
@@ -12,22 +12,30 @@ const Cards = () => {
 
 
     return (
-        <div className="">
-            <div className="cards-container">
-                <div className="card">
-                    <div className="cover-image">
-                        <img src={cards.cover_img} alt="" />
+        <div className="flex">
+            <div className="cards-container w-9/12 grid grid-cols-3 gap-3 items-center">
+                {
+                    cards.map((card) => (
+                        <div key={card.id} className="card h-80  border border-blue-700">
+                    <div className="cover-image w-full">
+                        <img src={card.cover_img} alt="" />
                     </div>
-                    <h2 className="course-tittle">{cards.course_name}</h2>
-                    <p className="course-details">{cards.course_details}</p>
+                    <h2 className="course-tittle">{card.course_name}</h2>
+                    <p className="course-details">{card.course_details}</p>
                     <div className="prize-credit flex justify-evenly">
-                        <h2>Prize: {cards.prize} $ </h2>
+                        <h2>Prize: {card.prize}$ </h2>
                         <img src="" alt="" className="bookmark-icon" />
-                        <h2 className="credit">Credit:{cards.credit}hr</h2>
+                        <h2 className="credit">Credit: {card.credit}hr</h2>
                     </div>
+                    <button className="select-btn w-full bg-blue-600 text-white">Select</button>
 
                 </div>
+                    ))
+                }
 
+            </div>
+            <div className="carts-container w-3/12">
+                <h1>this is cart area</h1>
             </div>
         </div>
     );
